b9b2eff2c1deef8d7d8f822250c3d145d3aced32
DonSanoj <sanojaminda@gmail.com>
2023-10-15 01:36:33 +0530

    Create animation for progress section

--- assets/js/index.js
+++ assets/js/index.js
@@ -19,3 +19,29 @@ document.querySelector('#search-icon').onclick = () => {
 document.querySelector('#search-form-close').onclick = () => {
     document.querySelector('#search-form').classList.remove('active');
 }
+
+// Progress section auto increment
+var reached = false;
+window.addEventListener('scroll', function () {
+    var element = document.getElementById('progress');
+    var position = element.getBoundingClientRect();
+
+    if (position.top < window.innerHeight && !reached) {
+        reached = true;
+        var counts = document.querySelectorAll('.count');
+        counts.forEach(function (counter) {
+            var max = parseInt(counter.getAttribute('max-data'));
+            var currentValue = 0;
+            var increment = max / 10;
+            var interval = 2300 / max;
+            var timer = setInterval(function () {
+                currentValue += increment;
+                if (currentValue >= max) {
+                    clearInterval(timer);
+                    currentValue = max;
+                }
+                counter.innerText = Math.round(currentValue);
+            }, interval);
+        });
+    }
+});
