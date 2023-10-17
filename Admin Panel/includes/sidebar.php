67b9e55b4c82f4007f525442d17cbe3fc944fa01
DonSanoj <sanojaminda@gmail.com>
2023-10-18 00:21:27 +0530

    Add log out part for Admin Panel

--- Admin Panel/includes/sidebar.php
+++ Admin Panel/includes/sidebar.php
@@ -21,11 +21,11 @@
             
             <li class="nav-item">
                 <a class="nav-link text-white " href="../pages/tables.html">
-                
+
                 <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-                    <i class="material-icons opacity-10">use</i>
+                    <i class="material-icons opacity-10">boy</i>
                 </div>
-                
+
                 <span class="nav-link-text ms-1">User Management</span>
                 </a>
             </li>
@@ -43,6 +43,28 @@
 
             <li class="nav-item mt-3">
                 <h6 class="ps-4 ms-2 text-uppercase text-xs text-white font-weight-bolder opacity-8">Account pages</h6>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link text-white " href="../pages/tables.html">
+
+                <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                    <i class="material-icons opacity-10">person</i>
+                </div>
+
+                <span class="nav-link-text ms-1">Profile</span>
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link text-white " href="/Login User and Admin page/logout.php">
+    
+                <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                    <i class="material-icons opacity-10">login</i>
+                </div>
+    
+                <span class="nav-link-text ms-1">Log Out</span>
+                </a>
             </li>
 
         </ul>
