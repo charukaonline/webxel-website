7d495e44224f4decc9f0dca009d8f58c4625b319
DonSanoj <sanojaminda@gmail.com>
2023-10-18 19:37:52 +0530

    added pages to admin panel

--- Admin Panel/includes/sidebar.php
+++ Admin Panel/includes/sidebar.php
@@ -2,7 +2,7 @@
     <div class="sidenav-header">
         <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
         <a class="navbar-brand m-0" href="../index.php">
-            <span class="ms-1 font-weight-bold text-white">WEBXEL Admin Panel</span>
+            <h5><span class="font-weight-bold text-white">W&#x039E;&#x042;X&#x039E;L Admin Panel</span></h5>
         </a>
     </div>
     <hr class="horizontal light mt-0 mb-2">
