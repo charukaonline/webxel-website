ae7c1a0e22645b8806fad6dcf4960766c379e768
DonSanoj <sanojaminda@gmail.com>
2023-10-18 18:49:26 +0530

    Update admin page

--- Admin Panel/includes/sidebar.php
+++ Admin Panel/includes/sidebar.php
@@ -1,7 +1,7 @@
 <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3   bg-gradient-dark" id="sidenav-main">
     <div class="sidenav-header">
         <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
-        <a class="navbar-brand m-0" href="#" target="_blank">
+        <a class="navbar-brand m-0" href="../index.php">
             <span class="ms-1 font-weight-bold text-white">Hi!,<?php echo $_SESSION['admin_name'] ?></span>
         </a>
     </div>
