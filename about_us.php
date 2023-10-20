354089790c0113d50ef634302c6de4d43203360f
ShayanHennedige <csthennedige@students.nsbm.ac.lk>
2023-10-20 13:08:57 +0530

    Update about us page

--- about_us.php
+++ about_us.php
@@ -3,54 +3,25 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="description"
-        content="WEBXEL, webxel, Technology Service, Technical Support, IT Service, web developing">
+    <meta name="description" content="WEBXEL, webxel, Technology Service, Technical Support, IT Service, web developing">
     <meta name="keywords" content="HTML,CSS,JavaScript,PHP">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 
-    <link rel="stylesheet" href="/assets/css/index.css">
     <link rel="stylesheet" href="/assets/css/about.css">
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
     <script src="https://kit.fontawesome.com/c4254e24a8.js"></script>
 
-    <title>WEBXEL - About Us</title>
-
+    <title>WEBXEL - About</title>
 </head>
 
 <body>
 
-    <!-- Header section start -->
-    <header>
-
-        <a href="index.html" class="logo">W&#x039E;&#x042;X&#x039E;L</a>
-
-        <nav class="navbar">
-            <a href="index.html">Home</a>
-            <a href="#">Services</a>
-            <a href="#">Contact Us</a>
-            <a href="#">About Us</a>
-        </nav>
-
-        <div class="icons">
-            <i class="fas fa-bars" id="menu-bars"></i>
-            <i class="fas fa-search" id="search-icon"></i>
-            <a href="#"><i class="fas fa-user" id="login-icon"></i></a>
-        </div>
-
-    </header>
-    <!-- Header section end -->
-
-    <!--Search form start-->
-    <form action="" id="search-form">
-        <input type="search" placeholder="Search here..." name="" id="search-box">
-        <label for="search-box" class="fas fa-search"></label>
-        <i class="fas fa-times" id="search-form-close"></i>
-    </form>
-    <!-- Search form end -->
+    <?php include ('navbar.php'); ?>
 
     <!-- About section start -->
     <section class="about">
@@ -168,53 +139,9 @@
     </section>
     <!-- About Section end -->
 
-    <!-- Footer section start -->
-    <footer class="footer">
-        <div class="footer_wrapper">
-            <ul>
-                <li>1. New services coming late 2024.</li>
-                <li>2. We've squashed some pesky bugs to ensure a smoother and more reliable experience for all users.
-                </li>
-                <li>3. Our team has worked on optimizing various aspects of W&#x039E;&#x042;X&#x039E;L, resulting in
-                    faster load times and improved efficiency.</li>
-            </ul>
+    <?php include ('footer.php'); ?>
 
-            <div class="footer_links">
-                <div class="footer_links_column">
-                    <div class="footer_links_column_title">Services</div>
-                    <a href="#">Graphics & Design</a>
-                    <a href="#">Web Development</a>
-                    <a href="#">Programming & Tech</a>
-                    <a href="#">Video & Animation</a>
-                    <a href="#">Photography</a>
-                    <a href="#">Music & Audio</a>
-                </div>
-
-                <div class="footer_links_column">
-                    <div class="footer_links_column_title">About</div>
-                    <a href="#">Our Team</a>
-                    <a href="#">Privacy Policy</a>
-                    <a href="#">Terms of Use</a>
-                </div>
-
-                <div class="footer_links_column">
-                    <div class="footer_links_column_title">Social Media</div>
-                    <a href="#"><i class='bx bxl-github'></i></a>
-                    <a href="#"><i class='bx bxl-linkedin'></i></a>
-                    <a href="#"><i class='bx bxl-facebook'></i></a>
-                    <a href="#"><i class='bx bxl-twitter'></i></a>
-                </div>
-            </div>
-
-            <hr>
-
-            <div class="footer-copyright">
-                <p>Copyright © W&#x039E;&#x042;X&#x039E;L 2023</p>
-            </div>
-
-        </div>
-    </footer>
-    <script>
+    <!-- <script>
         document.querySelector('.button').addEventListener('click', () => {
             document.querySelector('.footer-links').classList.add('active');
         });
@@ -225,7 +152,7 @@
             }
         });
 
-    </script>
+    </script> -->
 
 
     <!-- Footer section end -->
