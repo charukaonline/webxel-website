00c697317c3b7e5fc0a9fda6cca1e13ce2b5c394
charukaonline <nuwanthakonline@gmail.com>
2023-10-21 13:10:15 +0530

    services updated

--- services.php
+++ services.php
@@ -1,11 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
+    <meta name="description" content="WEBXEL, webxel, Technology Service, Technical Support, IT Service, web developing">
+    <meta name="keywords" content="HTML,CSS,JavaScript,PHP">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+    <link rel="stylesheet" href="/assets/css/about.css">
+    <link rel="stylesheet" href="/assets/css/index.css">
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+    <script src="https://kit.fontawesome.com/c4254e24a8.js"></script>
+
     <title>WEBXEL - Our Services</title>
 </head>
 <body>
-    
+
+    <?php include ('navbar.php'); ?>
+
+    <section class="services-top">
+        <div class="services-intro">
+            <h1>Our Services </h1>
+            <h3>Where Creativity Meets Technology.</h3>
+        </div>
+
+        <div class="services-desc">
+            At W&#x039E;&#x042;X&#x039E;L, We collaborate with companies small to large to solve problems,
+            seize opportunities and generate growth.
+        </div>
+    </section>
+
 </body>
 </html>
