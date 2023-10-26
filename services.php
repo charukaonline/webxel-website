1072ebfaa9f01ad69c3575f43780456d89933737
charukaonline <nuwanthakonline@gmail.com>
2023-10-26 13:41:18 +0530

    service page php update

--- services.php
+++ services.php
@@ -4,35 +4,143 @@
 <meta charset="UTF-8">
     <meta name="description" content="WEBXEL, webxel, Technology Service, Technical Support, IT Service, web developing">
     <meta name="keywords" content="HTML,CSS,JavaScript,PHP">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+	<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 
     <link rel="stylesheet" href="/assets/css/services.css">
     <link rel="stylesheet" href="/assets/css/index.css">
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
-    <script src="https://kit.fontawesome.com/c4254e24a8.js"></script>
 
-    <title>WEBXEL - Our Services</title>
+
+    
 </head>
 <body>
 
-    <?php include ('navbar.php'); ?>
+<?php include ('navbar.php'); ?>
 
-    <section class="services-top">
-        <div class="intro">
-            <h1>Our Services </h1>
-            <h3>Where Creativity Meets Technology.</h3>
-        </div>
+<div class="div1">
+			<div>
+			<h1>Our Services</h1>
+			<h4>We are unique and always ahead.</h4>
 
-        <div class="desc">
-            At W&#x039E;&#x042;X&#x039E;L, We collaborate with companies small to large to solve problems,
-            seize opportunities and generate growth.
-        </div>
-    </section>
+			<button>READ MORE</button>
+			<button>CONTACT US</button>
+
+			</div>	
+</div>
+<div class="div2">
+
+	
+		<img src="https://thimirathenuwara.com/assets/images/responsive-design.webp">
+		<div>
+			<h2>Web design & development</h2>
+		    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br> Nam pulvinar luctus sem, eget porta orci. Maecenas<br> molestie dui id diam feugiat, eu tincidunt mauris<br> aliquam. Duis commodo vitae ligula et interdum.<br> Maecenas faucibus mattis imperdiet. In rhoncus ac<br> ligula id ultricies.</p>
+		    	<button> MORE INFO</button>
+
+		</div>
+
+</div>
+
+<div class="div3">	
+	<div>
+		<h2>marketing & graphics</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br> Nam pulvinar luctus sem, eget porta orci. Maecenas<br> molestie dui id diam feugiat, eu tincidunt mauris<br> aliquam. Duis commodo vitae ligula et interdum.</p>
+    	<button>See It In Action</button>
+    </div>
+    	<img src="https://test.charukak.com/assets/flexible-portfolio.png">
+	
+</div>
+
+
+<div class="div4">
+	<h1>Mobile app development</h1>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus imperdiet rhoncus porta. Ut quis sem quis purus lobortis dictum. Aliquam<br>nec dignissim nisl. Vivamus cursus feugiat sapien, eget tincidunt leo ornare quis.</p>
+
+    <button>READ MORE</button>	
+</div>
+
+
+
+<div class="div5">
+	<h2>Custom Integrations</h2>
+<p>Phasellus sed nisi ac dui interdum semper. Etiam consequat fermentum sollicitudin. Fusce vulputate porta faucibus. Vivamus nulla tellus,<br> accumsan non efficitur id, pretium quis ante</p>
+<button> WHO WE ARE </button>	
+
+<img src="https://images.pexels.com/photos/106344/pexels-photo-106344.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
+
+</div>
+
+
+<div class="div6">
+	<h3>Our Latest Projects</h3>
+<p>Here is our latest projects. You'll love them!</p>
+</div>
+
+<div class="div7">
+		<div>
+			<img src="">
+		</div>
+		<div>
+			<img src="">
+		</div>
+		<div>
+			<img src="">
+		</div>
+		<div>
+			<img src="">
+		</div>
+</div>
+
+<div class="div7">
+		<div>
+			<img src="">
+		</div>
+		<div>
+			<img src="">
+		</div>
+		<div>
+			<img src="">
+		</div>
+		<div>
+			<img src="">
+		</div>
+</div>
+
+
+<div class="div7">
+		<div>
+			<img src="">
+		</div>
+		<div>
+			<img src="">
+		</div>
+</div>
+
+<div class="div8">
+	<h2>Our Vision & Mission</h2>
+<p>
+Nunc sit amet lobortis nulla. Nunc ullamcorper, mi id luctus dictum, augue tortor dictum ipsum, nec congue arcu <br>lorem in nisl. Duis neque lacus, viverra non mauris ac, pharetra rhoncus libero. Aliquam varius viverra ex, in venenatis<br> magna ornare sit amet. Integer varius sit amet turpis eu ullamcorper.</p>
+
+<div>
+	<center><img src="https://charukak.com/wp-content/uploads/2023/08/profile-pic-1.png"></center><h3>Charuka Karunarathna</h3>
+</div>
+
+
+</div>
+
+<div class="div9">
+	<h1>Wanna work with us?</h1>
+	<button> Contact Us </button>	
+
+</div>
+
+<?php include('footer.php'); ?>
+
+
 
 </body>
 </html>
