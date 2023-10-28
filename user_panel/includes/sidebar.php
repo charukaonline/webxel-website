1d6ca218ca559413f138562f70b362df311627f4
charukaonline <nuwanthakonline@gmail.com>
2023-10-29 01:43:54 +0530

    user panel added

---
+++ user_panel/includes/sidebar.php
@@ -0,0 +1,51 @@
+<section id="sidebar">
+    <a href="/Admin_Panel/admin_dashboard.php" class="brand">
+        <span class="text">W&#x039E;&#x042;X&#x039E;L Admin</span>
+    </a>
+    <ul class="side-menu top">
+        <li class="active">
+            <a href="/Admin_Panel/admin_dashboard.php">
+                <i class='bx bxs-dashboard'></i>
+                <span class="text">Dashboard</span>
+            </a>
+        </li>
+        <li>
+            <a href="/Admin_Panel/page_clone.php">
+                <i class='bx bxs-copy-alt'></i>
+                <span class="text">Page Clone</span>
+            </a>
+        </li>
+        <li>
+            <a href="/Admin_Panel/user_manage.php">
+                <i class='bx bxs-group'></i>
+                <span class="text">User Management</span>
+            </a>
+        </li>
+        <li>
+            <a href="/Admin_Panel/notification.php">
+                <i class='bx bxs-bell'></i>
+                <span class="text">Notification</span>
+            </a>
+        </li>
+    </ul>
+    <ul class="side-menu">
+        <li>
+            <a href="../index.php">
+                <i class='bx bxs-home'></i>
+                <span class="text">W&#x039E;&#x042;X&#x039E;L</span>
+            </a>
+        </li>
+        <li>
+            <a href="/Admin_Panel/admin_profile.php">
+                <i class='bx bxs-user'></i>
+                <span class="text">Profile</span>
+            </a>
+        </li>
+        <li>
+            <a href="/Login_User_and_Admin_page/logout.php" class="logout">
+                <i class='bx bx-log-out'></i>
+                <span class="text">Log Out</span>
+            </a>
+        </li>
+    </ul>
+</section>
