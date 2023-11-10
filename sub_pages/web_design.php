2394d7cd8f5dbe5a342e3d50b55c4bce1fbdc137
charukaonline <nuwanthakonline@gmail.com>
2023-11-10 13:59:53 +0530

    uploading web design php

---
+++ sub_pages/web_design.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>WEBXEL - Web Development</title>
+  <link rel="stylesheet" href="/assets/css/web-design/web-design.css">
+  <link rel="stylesheet" href="/assets/css/web-design/carousel.css">
+  <link rel="stylesheet" href="/assets/css/web-design/swiper-bundle.min.css">
+
+  <link rel="icon" type="image/png" href="./assets/images/logo2.jpg">
+
+</head>
+
+<body>
+
+<?php include('./includes/navbar.php'); ?>
+
+  <div class="explore-main-section top-section">
+    <div class="explore-title">
+      <h3>Build Your Dream Website</h3>
+    </div>
+  </div>
+
+  <div class="explore-content-section">
+    <div class="explore-top-left">
+      <h3> <span>Are you ready?</span> to launch your self</h3>
+      <p> At our Web Design Studio, we're passionate about crafting digital experiences that not only captivate but also convert. Our team of talented designers and developers works tirelessly to bring your vision to life, creating stunning, user-friendly websites that leave a lasting impression. <br><br>
+
+          Whether you're starting from scratch or looking to revamp your existing site, we're here to make your online presence shine. We specialize in responsive designs, ensuring your website looks and performs flawlessly across all devices. From sleek and modern aesthetics to intuitive navigation and seamless functionality, we've got it all covered. <br><br>
+
+          Our process is collaborative and transparent. We'll work closely with you to understand your unique needs and goals, translating them into a web design that not only looks fantastic but also functions seamlessly. Our attention to detail is unmatched, and we're committed to delivering results that exceed your expectations. When you choose our Web Design Studio, you're choosing a partner in your online success journey. Let's embark on this creative adventure together and turn your digital dreams into reality. Get in touch with us today, and let's make your website stand out in the digital landscape!"</p>
+    </div>
+    <div class="explore-top-right"></div>
+    <div class="explore-bottom-left"></div>
+    <div class="explore-bottom-right">
+      <p>In our Web Development section, we're dedicated to turning your digital ideas into dynamic, functional, and high-performing web solutions. Our team of experienced developers is passionate about transforming concepts into cutting-edge websites and web applications that not only meet your objectives but also exceed your expectations.<br><br>
+
+        Whether you need a custom-built website, an e-commerce platform, a robust web application, or any other digital solution, we've got the technical expertise to make it happen. We're committed to harnessing the latest technologies, coding best practices, and industry trends to ensure your project is on the forefront of digital innovation.<br><br>
+
+        We understand that each project is unique, and that's why we take a tailored approach. Our development process is collaborative, transparent, and fully customizable to your specific needs. From front-end design to back-end functionality, we meticulously craft every aspect to ensure a seamless and user-friendly experience.<br><br>
+
+        Our Web Development Hub is where your digital vision comes to life. We believe in the power of technology to elevate your online presence, and we're here to help you achieve your goals. Let's turn your ideas into functional digital reality. Get in touch with us today, and let's embark on a journey of innovation and success together!"</p>
+    </div>
+  </div>
+
+  <div class="explore-heading">
+    <h3>Our <span>offerings</span></h3>
+
+  </div>
+
+  <div class="slide-container swiper">
+    <div class="slide-content">
+      <div class="card-wrapper swiper-wrapper">
+        <div class="card swiper-slide">
+
+          <div class="card-content">
+            <h2 id="vp" class="vp name">Business</h2>
+            <p class="description">
+            <ul>
+              <li>-Design customization</li>
+              <li>-Responsive design</li>
+              <li>-Content upload</li>
+              <li>-Admin features</li>
+              <li>-Search engine optimization </li>
+              <li>-Analytics integration </li>
+            </ul>
+            </p>
+          </div>
+        </div>
+        <div class="card swiper-slide">
+
+
+          <div class="card-content">
+            <h2 class="name">Ecommerce</h2>
+            <p class="description">
+            <ul>
+              <li>-Single product page </li>
+              <li>-Cart page </li>
+              <li>-Checkout & My Account page </li>
+              <li>-Order tracking system </li>
+              <li>-Payment gateway </li>
+              <li>-Tax, shipping & email integration </li>
+            </ul>
+            </p>
+          </div>
+        </div>
+        <div class="card swiper-slide">
+
+          <div class="card-content">
+            <h2 class="name">LMS</h2>
+            <p class="description">
+            <ul>
+              <li>-Course content management</li>
+              <li>-Instructor management</li>
+              <li>-Student management</li>
+              <li>-Turnitin integrated</li>
+              <li>-Payment gateway</li>
+              <li>-Zoom / Recording support</li>
+            </ul>
+            </p>
+
+
+          </div>
+        </div>
+        <div class="card swiper-slide">
+
+
+          <div class="card-content">
+            <h2 class="name">WordPress</h2>
+            <p class="description">
+            <ul>
+              <li>-Easy maintenance</li>
+              <li>-Responsive design</li>
+              <li>-Rich admin features</li>
+              <li>-Plugin support</li>
+              <li>-Regular updates</li>
+              <li>SEO friendly</li>
+            </ul>
+            </p>
+
+          </div>
+        </div>
+        <div class="card swiper-slide">
+
+
+          <div class="card-content">
+            <h2 class="name">POS</h2>
+            <p class="description">
+            <ul>
+              <li>-Sales management</li>
+              <li>-Product management</li>
+              <li>-Payment gateway</li>
+              <li>-Inventory management</li>
+              <li>-Easy maintenance</li>
+              <li>-Customer preferences tracking</li>
+            </ul>
+            </p>
+
+
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+    <div class="swiper-button-next swiper-navBtn"></div>
+    <div class="swiper-button-prev swiper-navBtn"></div>
+    <div class="swiper-pagination"></div>
+  </div>
+
+  <!-- Swiper JS -->
+  <script src="/assets/js/web-design/swiper-bundle.min.js"></script>
+
+  <!-- JavaScript -->
+  <script src="/assets/js/web-design/web-design.js"></script>
+
+
+
+  <?php include('./includes/footer.php'); ?>
+
+
+</body>
+
+</html>
