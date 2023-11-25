0ddd2a6335b2e7c91eb5e117772f721c4a934be2
DonSanoj <sanojaminda@gmail.com>
2023-11-26 01:04:10 +0530

    Update sub pages

--- sub_pages/marketing_advertising.php
+++ sub_pages/marketing_advertising.php
@@ -1,5 +1,147 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-	
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="WEBXEL, webxel, IT Service">
+    <meta name="keywords" content="HTML,CSS,JavaScript,PHP">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-?>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+    <link rel="stylesheet" href="../assets/css/sub-pages/marketing-advertising/marketing_advertising.css">
+    <link rel="stylesheet" href="../assets/css/header.css">
+    <link rel="stylesheet" href="../assets/css/footer.css">
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+    <script src="https://kit.fontawesome.com/c4254e24a8.js"></script>
+
+    <title>WEBXEL - Marketing and Advertising</title>
+    <link rel="icon" href="/assets/images/logo2.jpg" type="image/jpg">">
+</head>
+
+<body>
+
+    <?php include('../includes/navbar.php') ?>
+
+    <section class="intro">
+
+        <div class="text-box">
+
+            <h1>Marketing and Advertising</h1>
+            <h3>Strategic Brand Amplification Hub: Marketing and Advertising Excellence</h3>
+            <a href="../sub_pages/order.php">Place Order Now</a>
+
+        </div>
+
+    </section>
+
+
+    <div class="grad">
+
+        <section class="course">
+            <h1>Packages We Offer</h1>
+            <h2>Thank you for considering our services for your app design needs. </h2>
+
+            <div class="row">
+                <div class="course-col">
+                    <h3>Silver</h3>
+                    <h5>Starting at $5,880</h5>
+                    <p>
+                        Landing Page<br>
+                        UI/UX Design Stage<br>
+                        iOS Development<br>
+                        Android Development<br>
+                        Custom Server Development<br>
+                    </p>
+                </div>
+                <div class="course-col">
+                    <h3>Gold</h3>
+                    <h5>Starting at $34,100</h5>
+                    <p>
+                        Market Demand Verification<br>
+                        Vision & Strategy Workshops<br>
+                        Wire-frames & Clickable Prototype<br>
+                        User Interviews Report<br>
+                        Cost Estimation<br>
+                    </p>
+                </div>
+                <div class="course-col">
+                    <h3>Platinum </h3>
+                    <h5> Starting at $ 50,000 </h5>
+                    <p> Native App<br>
+                        Product Definition<br>
+                        Product Design (UX & UI)<br>
+                        Figma Prototype<br>
+                        Delivery Time - Phase 1
+                        6 to 8 weeks
+                    </p>
+                </div>
+            </div>
+
+        </section>
+
+        <section class="work">
+
+            <h1>Work we have done</h1>
+            <h2>We are pleased to provide you an idea about our work with the following.</h2>
+
+            <div class="row">
+                <div class="work-col">
+                    <img src="../assets/images/app-development/image09.jpg">
+                    <div class="layer">
+                        <h3>Desktop Apps</h3>
+                    </div>
+                </div>
+                <div class="work-col">
+                    <img src="../assets/images/app-development/image03.jpg">
+                    <div class="layer">
+                        <h3>Native Mobile Apps</h3>
+                    </div>
+                </div>
+                <div class="work-col">
+                    <img src="../assets/images/app-development/image07.jpg">
+                    <div class="layer">
+                        <h3>Legacy Apps</h3>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="moreOp">
+            <h1>Explore More</h1>
+            <h2>Unlocking possibilities in the digital realm, where innovation meets creativity.</h2>
+
+            <div class="line">
+                <div class="moreOp-col">
+                    <img src="../assets/images/app-development/image12.jpg">
+                    <h3>Developer Guides</h3>
+
+                    <p>Find training classes that describe how to accomplish a specific task with code samples you can
+                        re-use in your app</p>
+
+                </div>
+                <div class="moreOp-col">
+                    <img src="../assets/images/app-development/image13.jpg">
+                    <h3>Sample Code</h3>
+                    <p>Jump-start your development using these sample projects</p>
+                </div>
+                <div class="moreOp-col">
+                    <img src="../assets/images/app-development/image14.jpg">
+                    <h3>Quality guide lines</h3>
+                    <p>Build a high quality app with these design and behavior successfully</p>
+                </div>
+
+            </div>
+
+        </section>
+
+    </div>
+
+    <?php include('../includes/footer.php') ?>
+
+</body>
+
+</html>
