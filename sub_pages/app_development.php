e058637e2f56b90ff94e2ba5a5b2af23f89123a2
charukaonline <nuwanthakonline@gmail.com>
2023-11-27 13:53:30 +0530

    chat feature for sub pages

--- sub_pages/app_development.php
+++ sub_pages/app_development.php
@@ -22,6 +22,20 @@
     <link rel="icon" href="/assets/images/logo2.jpg" type="image/jpg">
 </head>
 
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/655b9f0991e5c13bb5b1f4be/1hfmuafig';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+    </script>
+    <!--End of Tawk.to Script-->
+
 <body>
 
     <?php include('../includes/navbar.php'); ?>
